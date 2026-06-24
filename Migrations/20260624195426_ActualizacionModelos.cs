e1b98c79a3d941d4aea399b1d3d0b45a2ad89fbb
nils <nilsolivermachacamamani@gmail.com>
2026-06-24 15:54:48 -0400

    Completarh

---
+++ Migrations/20260624195426_ActualizacionModelos.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LOGIN.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizacionModelos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rol",
+                table: "Usuarios",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: true,
+                defaultValue: "Usuario");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rol",
+                table: "Usuarios");
+        }
+    }
+}
