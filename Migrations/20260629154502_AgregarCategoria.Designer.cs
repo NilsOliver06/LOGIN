d87b268d438f69bd149b66d3827de089db5615a8
nils <nilsolivermachacamamani@gmail.com>
2026-06-29 12:01:13 -0400

    bola11

--- Migrations/20260629154502_AgregarCategoria.Designer.cs
+++ Migrations/20260629154502_AgregarCategoria.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LOGIN.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LOGIN.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260629154502_AgregarCategoria")]
+    partial class AgregarCategoria
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
